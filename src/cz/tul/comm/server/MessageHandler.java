12f9638eab2a2a603ab0444702ff1016f4ef5b96
Petr Ječmen <Petr Jemen@PJ>
2013-02-21 09:45:14 +0100

    nemusi se naslouchat na IP, lze i dle UUID

--- src/cz/tul/comm/server/MessageHandler.java
+++ src/cz/tul/comm/server/MessageHandler.java
@@ -6,6 +6,7 @@ import cz.tul.comm.Message;
 import java.net.InetAddress;
 import java.util.Map;
 import java.util.Queue;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.logging.Level;
@@ -13,18 +14,21 @@ import java.util.logging.Logger;
 
 /**
  * Middle man between server socket and registered handlers. Stores received
- * data in queues, from which handlers can obtain data.
- * Handler is notified each time data object is received.
+ * data in queues, from which handlers can obtain data. Handler is notified each
+ * time data object is received.
+ *
  * @author Petr Ječmen
  */
 public class MessageHandler extends Thread implements IMessageHandler, IResponseHandler {
 
     private static final Logger log = Logger.getLogger(MessageHandler.class.getName());
-    private final Map<InetAddress, Object> handlers;
+    private final Map<InetAddress, Object> handlersAddress;
+    private final Map<UUID, Object> handlersId;
     private final Map<Object, Queue<Object>> messages;
 
     public MessageHandler() {
-        handlers = new ConcurrentHashMap<>();
+        handlersAddress = new ConcurrentHashMap<>();
+        handlersId = new ConcurrentHashMap<>();
         messages = new ConcurrentHashMap<>();
     }
 
@@ -36,8 +40,8 @@ public class MessageHandler extends Thread implements IMessageHandler, IResponse
 
                 // TODO check for system message
             }
-            if (handlers.containsKey(address)) {
-                final Object owner = handlers.get(address);
+            if (handlersAddress.containsKey(address)) {
+                final Object owner = handlersAddress.get(address);
                 messages.get(owner).add(msg);
 
                 synchronized (owner) {
@@ -51,11 +55,25 @@ public class MessageHandler extends Thread implements IMessageHandler, IResponse
 
     @Override
     public void registerResponse(final InetAddress address, final Object owner) {
-        if (handlers.containsKey(address)) {
-            log.log(Level.WARNING, "Handler fot IP {0} already assigned, no changes made.", address.getHostAddress());
+        if (handlersAddress.containsKey(address)) {
+            log.log(Level.WARNING, "Handler for IP {0} already assigned, no changes made.", address.getHostAddress());
         } else {
-            handlers.put(address, owner);
-            messages.put(owner, new ConcurrentLinkedQueue<>());
+            handlersAddress.put(address, owner);
+            if (!messages.containsKey(owner)) {
+                messages.put(owner, new ConcurrentLinkedQueue<>());
+            }
+        }
+    }
+
+    @Override
+    public void registerResponse(final UUID id, final Object owner) {
+        if (handlersId.containsKey(id)) {
+            log.log(Level.WARNING, "Handler for UUID {0} already assigned, no changes made.", id.toString());
+        } else {
+            handlersId.put(id, owner);
+            if (!messages.containsKey(owner)) {
+                messages.put(owner, new ConcurrentLinkedQueue<>());
+            }
         }
     }
 
@@ -67,12 +85,27 @@ public class MessageHandler extends Thread implements IMessageHandler, IResponse
 
     @Override
     public void deregisterResponse(final InetAddress address, final Object owner) {
-        Object own = handlers.get(address);
+        Object own = handlersAddress.get(address);
         if (own == owner) {
-            handlers.remove(address);
-            messages.remove(own);
+            handlersAddress.remove(address);
+            if (!handlersId.containsValue(own)) {
+                messages.remove(own);
+            }
         } else {
             log.log(Level.WARNING, "Wrong owner for IP {0}, no changes made", address.getHostAddress());
+        }
+    }
+
+    @Override
+    public void deregisterResponse(final UUID id, final Object owner) {
+        Object own = handlersId.get(id);
+        if (own == owner) {
+            handlersId.remove(id);
+            if (!handlersAddress.containsValue(own)) {
+                messages.remove(own);
+            }
+        } else {
+            log.log(Level.WARNING, "Wrong owner for UUID {0}, no changes made", id.toString());
         }
     }
 }
--- src/cz/tul/comm/socket/IResponseHandler.java
+++ src/cz/tul/comm/socket/IResponseHandler.java
@@ -2,6 +2,7 @@ package cz.tul.comm.socket;
 
 import java.net.InetAddress;
 import java.util.Queue;
+import java.util.UUID;
 
 /**
  * Interface for classes used to register listeners for client answers.
@@ -11,7 +12,11 @@ public interface IResponseHandler {
 
     void registerResponse(final InetAddress address, final Object owner);
 
+    void registerResponse(final UUID id, final Object owner);
+
     void deregisterResponse(final InetAddress address, final Object owner);
+
+    void deregisterResponse(final UUID id, final Object owner);
 
     Queue<Object> getResponseQueue(final Object owner);
 
