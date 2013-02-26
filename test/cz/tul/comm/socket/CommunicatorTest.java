fd06ba6b4efa6dc52d0a3964bb4a956b25b0e1ea
Petr Ječmen <petr.jecmen@tul.cz>
2013-02-26 10:35:33 +0100

    unit test Communicatoru. Closes #018

---
+++ test/cz/tul/comm/socket/CommunicatorTest.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.tul.comm.socket;
+
+import java.net.InetAddress;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Petr Ječmen
+ */
+public class CommunicatorTest {
+
+    private static final int PORT_SERVER = 65432;
+    private static ServerSocket ss;
+
+    public CommunicatorTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+        ss = ServerSocket.createServerSocket(PORT_SERVER);
+    }
+
+    @AfterClass
+    public static void tearDownClass() {        
+        ss.stopService();
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getAddress method, of class Communicator.
+     */
+    @Test
+    public void testCommunicator() {
+        System.out.println("Communicator");
+
+        final InetAddress ip = InetAddress.getLoopbackAddress();
+        final Communicator instance = new Communicator(ip, PORT_SERVER);
+
+        assertEquals(ip, instance.getAddress());
+
+        final boolean result = instance.sendData("test");
+        assertTrue("Target could not receive data", result);
+    }
+}
