2aa8092f7e4020f424461a599cb11c2b260b6703
Petr Ječmen <petr.jecmen@tul.cz>
2013-08-29 10:31:16 +0200

    Oprava testu historie (vynechat logout zpravu).

--- test/cz/tul/comm/history/HistoryTest.java
+++ test/cz/tul/comm/history/HistoryTest.java
@@ -143,6 +143,8 @@ public class HistoryTest {
                 }
             }
 
+            s.getHistory().enable(false);
+            
             c.stopService();
             s.stopService();
 
