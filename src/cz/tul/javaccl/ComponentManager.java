04048edfee42e6e9c775d7ab04aa6ad2db9b9a63
Petr Ječmen <petr.jecmen@tul.cz>
2013-11-07 13:12:25 +0100

    Doplneni javadocs.

--- src/cz/tul/javaccl/ComponentManager.java
+++ src/cz/tul/javaccl/ComponentManager.java
@@ -3,13 +3,22 @@ package cz.tul.javaccl;
 import cz.tul.javaccl.socket.IDFilter;
 
 /**
+ * Interface allowing enabling / disabling of some library components.
  *
  * @author Petr Jecmen
  */
 public interface ComponentManager {
 
+    /**
+     *
+     * @param filter new filter of client IDs
+     */
     void setIdFilter(final IDFilter filter);
-    
+
+    /**
+     *
+     * @param enable true to enable automatic server / client discovery
+     */
     void enableDiscoveryDaemon(final boolean enable);
-    
+
 }
