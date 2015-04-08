7eecdd681b45720f94df65915b208d5957430c8a
Petr Ječmen <petr.jecmen@tul.cz>
2015-04-08 14:10:57 +0200

    Add response field to be able to better analyze the exception cause.

--- src/cz/tul/javaccl/exceptions/ConnectionException.java
+++ src/cz/tul/javaccl/exceptions/ConnectionException.java
@@ -9,21 +9,43 @@ package cz.tul.javaccl.exceptions;
 public class ConnectionException extends Exception {
 
     private final ConnectionExceptionCause exceptionCause;
+    private final String response;
 
     /**
      * Creates a new instance of <code>ConnectionException</code> withgiven
      * cause.
      *
      * @param cause cause of the exception
+     * @param response response from the opposite side
      */
-    public ConnectionException(ConnectionExceptionCause cause) {
+    public ConnectionException(ConnectionExceptionCause cause, String response) {
         this.exceptionCause = cause;
+        this.response = response;
     }
+    
+    /**
+     * Creates a new instance of <code>ConnectionException</code> withgiven
+     * cause.
+     *
+     * @param cause cause of the exception
+     */
+    public ConnectionException(ConnectionExceptionCause cause) {        
+        this(cause, "No Response");
+    }    
 
     /**
      * @return cause of the exception
      */
     public ConnectionExceptionCause getExceptionCause() {
         return exceptionCause;
+    }
+    
+    public String getResponse() {
+        return response;
+    }
+    
+    @Override
+    public String toString() {
+        return exceptionCause.toString() + ": " + response;
     }
 }
